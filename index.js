52bc70c18988c64be291fbc189e6dfc37253867a
Brett Karr <b.karr@polyclip.us>
2020-01-22 21:21:08 -0600

    blearg

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // code away!
+require("dotenv").config();
 const server = require("./server.jsx");
-const port = 7779;
+//hard coding the port number for deployment.
+//const port = 7779;
+/*
+$ env => gives pairing environments
+** none for port
+$ export PORT = ####
+** inserts PORT = #### into env
+*/
+const port = process.env.PORT || 7780;
 server.listen(port, () => console.log(`\n**** LISTENING ON PORT ${port} ****`));
